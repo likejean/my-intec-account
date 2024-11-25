a72147942088526ebd9e39806a95350df90c9180
likejean <popachs@yahoo.com>
2024-11-24 18:53:03 -0800

    Added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Record from './components/Note/Record';
 import Profile from './components/Profile/Profile';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
 	return (
-		<div className="app-wrapper">
-			<Header />
-			<Navbar />
-			<div>
-				<Profile />
-				<Record />
+		<BrowserRouter>			
+			<div className="app-wrapper">
+				<Header />
+				<Navbar />				
+				<Routes>
+					<Route path="/notes" element={<Record />} />
+					<Route path="/profile" element={<Profile />} />
+				</Routes>
 			</div>
 			
-		</div>
+		</BrowserRouter>
 	);
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,10 +2,12 @@
 import React from 'react';
 import css from './Navbar.module.css';
 
+
 const Navbar = () => {
 	return (
 		<div className={css.nav}>
 			<ul>
+				
 				<li>
 					<a href="/profile">Profile</a>
 				</li>
